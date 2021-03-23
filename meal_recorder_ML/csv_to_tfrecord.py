bb1e928fda14738c373d6d63bdeeaf15c8b4b42a
Loc Hoang <hpkhanhloc@gmail.com>
2021-03-23 15:04:28 +0200

    feat: add notebook and python code for building model

---
+++ meal_recorder_ML/csv_to_tfrecord.py
@@ -0,0 +1,123 @@
+"""
+Usage:
+  # From tensorflow/models/research
+  # Create train data:
+  python csv_to_tfrecord.py --csv_input=train_labels.csv  --output_path=train.record
+  # Create test data:
+  python csv_to_tfrecord.py --csv_input=test_labels.csv  --output_path=test.record
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import os
+import io
+import pandas as pd
+
+from tensorflow.python.framework.versions import VERSION
+if VERSION >= "2.0.0a0":
+    import tensorflow.compat.v1 as tf
+else:
+    import tensorflow as tf
+
+from PIL import Image
+from object_detection.utils import dataset_util
+from collections import namedtuple, OrderedDict
+
+flags = tf.app.flags
+flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
+flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+FLAGS = flags.FLAGS
+
+
+def class_text_to_int(row_label):
+    if row_label == 'rice':
+        return 1
+    elif row_label == 'croissant':
+        return 2
+    elif row_label == 'hamburger':
+        return 3
+    elif row_label == 'pizza':
+        return 4
+    elif row_label == 'sandwiches':
+        return 5
+    elif row_label == 'fried chicken':
+        return 6
+    elif row_label == 'steak':
+        return 7
+    elif row_label == 'simmered pork':
+        return 8
+    elif row_label == 'green salad':
+        return 9:
+    elif row_label == 'french fries':
+        return 10
+    else:
+        return 0
+
+
+def split(df, group):
+    data = namedtuple('data', ['path', 'object'])
+    gb = df.groupby(group)
+    return [data(path, gb.get_group(x)) for path, x in zip(gb.groups.keys(), gb.groups)]
+
+
+def create_tf_example(group, path):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.path)), 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
+    print(width, height)
+
+    filename = group.object.iloc[0]["filename"].encode('utf8')
+    print(filename)
+    image_format = b'jpg'
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+    classes_text = []
+    classes = []
+
+    for index, row in group.object.iterrows():
+        xmins.append(row['xmin'] / width)
+        xmaxs.append(row['xmax'] / width)
+        ymins.append(row['ymin'] / height)
+        ymaxs.append(row['ymax'] / height)
+        classes_text.append(row['class'].encode('utf8'))
+        classes.append(class_text_to_int(row['class']))
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+
+def main(_):
+    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    path = os.path.join(os.getcwd())
+    examples = pd.read_csv(FLAGS.csv_input)
+    grouped = split(examples, 'path')
+    for group in grouped:
+        tf_example = create_tf_example(group, path)
+        writer.write(tf_example.SerializeToString())
+
+    writer.close()
+    output_path = os.path.join(os.getcwd(), FLAGS.output_path)
+    print('Successfully created the TFRecords: {}'.format(output_path))
+
+
+if __name__ == '__main__':
+    tf.app.run()
